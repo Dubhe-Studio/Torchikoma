d06b59570b85d6a3d8ded5882cbd1210e8f63099
Gu_ZT <administration@ztxy666.cn>
2022-05-02 21:42:02 +0800

    update

--- src/main/java/dev/dubhe/torchikoma/registry/MyItems.java
+++ src/main/java/dev/dubhe/torchikoma/registry/MyItems.java
@@ -19,6 +19,11 @@ public class MyItems { //TODO 不稳定, 必须在方块注册事件之后初始
     public static final Item PRISMARINE_TORCH = new BlockItem(MyBlocks.PRISMARINE_TORCH, defaultProperties()).setRegistryName(Torchikoma.getId("prismarine_torch"));
     public static final Item GLOWSTONE_TORCH = new BlockItem(MyBlocks.GLOWSTONE_TORCH, defaultProperties()).setRegistryName(Torchikoma.getId("glowstone_torch"));
     public static final Item TORCH_LAUNCHER = new Item(defaultProperties()).setRegistryName(Torchikoma.getId("torch_launcher"));
+    public static final Item CLUSTERED_TORCH = new Item(defaultProperties()).setRegistryName(Torchikoma.getId("clustered_torch"));
+    public static final Item CLUSTERED_REDSTONE_TORCH = new Item(defaultProperties()).setRegistryName(Torchikoma.getId("clustered_redstone_torch"));
+    public static final Item CLUSTERED_SOUL_TORCH = new Item(defaultProperties()).setRegistryName(Torchikoma.getId("clustered_soul_torch"));
+    public static final Item CLUSTERED_GLOWSTONE_TORCH = new Item(defaultProperties()).setRegistryName(Torchikoma.getId("clustered_glowstone_torch"));
+    public static final Item CLUSTERED_PRISMARINE_TORCH = new Item(defaultProperties()).setRegistryName(Torchikoma.getId("clustered_prismarine_torch"));
 
     private static Item.Properties defaultProperties() {
         return new Item.Properties().tab(TAB);
