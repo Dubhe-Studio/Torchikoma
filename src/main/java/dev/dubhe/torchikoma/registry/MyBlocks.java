9e541159bdbb93ce4de2a004911650b78c6017b9
Cjsah <2684117397@qq.com>
2022-05-02 22:59:29 +0800

    墙壁火把修复

--- src/main/java/dev/dubhe/torchikoma/registry/MyBlocks.java
+++ src/main/java/dev/dubhe/torchikoma/registry/MyBlocks.java
@@ -5,6 +5,7 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.TorchBlock;
+import net.minecraft.world.level.block.WallTorchBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 
@@ -12,13 +13,13 @@ public class MyBlocks {
     public static final Block PRISMARINE_TORCH = new TorchBlock(defaultProperties(Material.DECORATION)
             .noCollission().instabreak().sound(SoundType.WOOD).lightLevel(state -> 15), ParticleTypes.FLAME)
             .setRegistryName(Torchikoma.getId("prismarine_torch"));
-    public static final Block PRISMARINE_WALL_TORCH = new TorchBlock(defaultProperties(Material.DECORATION)
+    public static final Block PRISMARINE_WALL_TORCH = new WallTorchBlock(defaultProperties(Material.DECORATION)
             .noCollission().instabreak().sound(SoundType.WOOD).lootFrom(() -> PRISMARINE_TORCH).lightLevel(state -> 15), ParticleTypes.FLAME)
             .setRegistryName(Torchikoma.getId("prismarine_wall_torch"));
     public static final Block GLOWSTONE_TORCH = new TorchBlock(defaultProperties(Material.DECORATION)
             .noCollission().instabreak().sound(SoundType.WOOD).lightLevel(state -> 15), ParticleTypes.FLAME)
             .setRegistryName(Torchikoma.getId("glowstone_torch"));
-    public static final Block GLOWSTONE_WALL_TORCH = new TorchBlock(defaultProperties(Material.DECORATION)
+    public static final Block GLOWSTONE_WALL_TORCH = new WallTorchBlock(defaultProperties(Material.DECORATION)
             .noCollission().instabreak().sound(SoundType.WOOD).lootFrom(() -> GLOWSTONE_TORCH).lightLevel(state -> 15), ParticleTypes.FLAME)
             .setRegistryName(Torchikoma.getId("glowstone_wall_torch"));
 
