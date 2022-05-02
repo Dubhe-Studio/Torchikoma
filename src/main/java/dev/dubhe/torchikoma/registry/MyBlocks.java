58386b119f0694c4b275ede8e2e33cfbbe781189
Cjsah <2684117397@qq.com>
2022-05-02 21:41:30 +0800

    update

--- src/main/java/dev/dubhe/torchikoma/registry/MyBlocks.java
+++ src/main/java/dev/dubhe/torchikoma/registry/MyBlocks.java
@@ -1,19 +1,19 @@
 package dev.dubhe.torchikoma.registry;
 
 import dev.dubhe.torchikoma.Torchikoma;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.TorchBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 
 public class MyBlocks {
 
-    public static final Block PRISMARINE_TORCH = new Block(defaultProperties(Material.STONE)).setRegistryName(Torchikoma.getId("prismarine_torch"));
-    public static final Block GLOWSTONE_TORCH = new Block(defaultProperties()).setRegistryName(Torchikoma.getId("glowstone_torch"));
-
-    private static BlockBehaviour.Properties defaultProperties() {
-        return defaultProperties(Material.METAL);
-    }
+    public static final Block PRISMARINE_TORCH = new TorchBlock(defaultProperties(Material.DECORATION).noCollission().instabreak().lightLevel(state -> 15), ParticleTypes.FLAME)
+            .setRegistryName(Torchikoma.getId("prismarine_torch"));
+    public static final Block GLOWSTONE_TORCH = new TorchBlock(defaultProperties(Material.DECORATION).noCollission().instabreak().lightLevel(state -> 15), ParticleTypes.FLAME)
+            .setRegistryName(Torchikoma.getId("glowstone_torch"));
 
     private static BlockBehaviour.Properties defaultProperties(Material material) {
         return BlockBehaviour.Properties.of(material).sound(SoundType.METAL).strength(5.0F).requiresCorrectToolForDrops();
--- src/main/java/dev/dubhe/torchikoma/registry/MyItems.java
+++ src/main/java/dev/dubhe/torchikoma/registry/MyItems.java
@@ -1,14 +1,13 @@
 package dev.dubhe.torchikoma.registry;
 
 import dev.dubhe.torchikoma.Torchikoma;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 
-public class MyItems {
+public class MyItems { //TODO 不稳定, 必须在方块注册事件之后初始化, 否则NPE异常
     private static final CreativeModeTab TAB = new CreativeModeTab(Torchikoma.ID + ".tab") {
         @NotNull
         @Override
@@ -17,11 +16,8 @@ public class MyItems {
         }
     };
 
-
-
-
-    public static final Item PRISMARINE_TORCH = new Item(defaultProperties()).setRegistryName(Torchikoma.getId("prismarine_torch"));
-    public static final Item GLOWSTONE_TORCH = new Item(defaultProperties()).setRegistryName(Torchikoma.getId("glowstone_torch"));
+    public static final Item PRISMARINE_TORCH = new BlockItem(MyBlocks.PRISMARINE_TORCH, defaultProperties()).setRegistryName(Torchikoma.getId("prismarine_torch"));
+    public static final Item GLOWSTONE_TORCH = new BlockItem(MyBlocks.GLOWSTONE_TORCH, defaultProperties()).setRegistryName(Torchikoma.getId("glowstone_torch"));
     public static final Item TORCH_LAUNCHER = new Item(defaultProperties()).setRegistryName(Torchikoma.getId("torch_launcher"));
 
     private static Item.Properties defaultProperties() {
