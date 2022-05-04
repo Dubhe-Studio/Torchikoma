548a7acef6ec6e8ade458bfd43494facb7984c01
Cjsah <2684117397@qq.com>
2022-05-04 22:31:50 +0800

    进一步完成gui

--- src/main/java/dev/dubhe/torchikoma/menu/TorchLauncherMenu.java
+++ src/main/java/dev/dubhe/torchikoma/menu/TorchLauncherMenu.java
@@ -1,31 +1,170 @@
 package dev.dubhe.torchikoma.menu;
 
+import dev.dubhe.torchikoma.item.TorchLauncher;
 import dev.dubhe.torchikoma.registry.MyMenuTypes;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.world.Container;
+import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class TorchLauncherMenu extends AbstractContainerMenu {
+public class TorchLauncherMenu extends AbstractContainerMenu implements Container {
+    public static int temp1 = 0;
+    public static int temp2 = 0;
+    public static int temp3 = 0;
+    public static int temp4 = 0;
+    private final NonNullList<ItemStack> items = NonNullList.withSize(5, ItemStack.EMPTY);
+    private int gunpowder = 0;
+    private final ItemStack itemStack;
 
-    public TorchLauncherMenu(int pContainerId, Inventory inventory) {
+    public TorchLauncherMenu(int pContainerId, Inventory inventory, ItemStack itemStack) {
         super(MyMenuTypes.TORCH_LAUNCHER, pContainerId);
+        this.itemStack = itemStack;
+        this.startOpen(inventory.player);
+        this.loadData();
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                this.addSlot(new Slot(this, j + i * 2, 134 + j * 18, 16 + i * 18));
+            }
+        }
+        this.addSlot(new Slot(this, 4, 51, 25));
 
         for(int i = 0; i < 3; ++i) {
             for(int j = 0; j < 9; ++j) {
-                this.addSlot(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18 + i));
+                this.addSlot(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
             }
         }
 
         for(int i = 0; i < 9; ++i) {
-            this.addSlot(new Slot(inventory, i, 8 + i * 18, 140));
+            this.addSlot(new Slot(inventory, i, 8 + i * 18, 142));
         }
+    }
+
+    private void loadData() {
+        if (itemStack.isEmpty()) return;
+        CompoundTag nbt = itemStack.getOrCreateTag();
+        if (!itemStack.hasTag() || !nbt.contains("Torches") || !nbt.contains("Gunpowder")) {
+            nbt.put("Torches", new ListTag());
+            nbt.putInt("Gunpowder", 0);
+            this.items.clear();
+            this.gunpowder = 0;
+        } else {
+            ListTag torches = nbt.getList("Torches", 10);
+            CompoundTag subNbt;
+            int j;
+            for (int i = 0; i < torches.size(); i++) {
+                subNbt = torches.getCompound(i);
+                j = subNbt.getByte("Slot") & 255;
+                if (j < this.items.size()) this.items.set(j, ItemStack.of(subNbt));
+            }
+            this.gunpowder = nbt.getInt("Gunpowder");
+        }
+    }
+
+    private void saveData() {
+        if (this.itemStack.isEmpty()) return;
+        ListTag torches = new ListTag();
+        CompoundTag nbt;
+        ItemStack item;
+        for (int i = 0; i < this.items.size(); i++) {
+            item = this.items.get(i);
+            if (!item.isEmpty()) {
+                nbt = item.save(new CompoundTag());
+                nbt.putByte("Slot", (byte) i);
+                torches.add(nbt);
+            }
+        }
+        this.itemStack.getOrCreateTag().put("Torches", torches);
+        this.itemStack.getOrCreateTag().putInt("Gunpowder", this.gunpowder);
     }
 
     @Override
     public boolean stillValid(Player pPlayer) {
         return true;
     }
+
+    @Override
+    public int getContainerSize() {
+        loadData();
+        return this.items.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        loadData();
+        return this.items.isEmpty();
+    }
+
+    @Override
+    public ItemStack getItem(int pIndex) {
+        loadData();
+        return this.items.get(pIndex);
+    }
+
+    @Override
+    public ItemStack removeItem(int pIndex, int pCount) {
+        loadData();
+        ItemStack stack = !this.items.get(pIndex).isEmpty() ? ContainerHelper.removeItem(this.items, pIndex, pCount) : ItemStack.EMPTY;
+        saveData();
+        return stack;
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int pIndex) {
+        loadData();
+        ItemStack itemstack = this.items.get(pIndex);
+        this.items.set(pIndex, ItemStack.EMPTY);
+        saveData();
+        return itemstack;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public boolean isEmpty(int pIndex) {
+        loadData();
+        return this.items.get(pIndex).isEmpty();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public int getGunpowder() {
+        return Math.min(this.gunpowder, 100);
+    }
+
+    @Override
+    public void setItem(int pIndex, ItemStack pStack) {
+        loadData();
+        this.items.set(pIndex, pStack);
+        if (pIndex == 4) {
+            ItemStack item = this.items.get(pIndex);
+            if (this.gunpowder <= 84 && !item.isEmpty()) {
+                int count = Math.min((100 - this.gunpowder) / 16, item.getCount());
+                item.shrink(count);
+                this.gunpowder += count * 16;
+            }
+        }
+        saveData();
+    }
+
+    @Override
+    public void setChanged() {
+    }
+
+    @Override
+    public void removed(Player pPlayer) {
+        super.removed(pPlayer);
+        this.stopOpen(pPlayer);
+    }
+
+    @Override
+    public void clearContent() {
+        this.items.clear();
+    }
+
 }
--- src/main/java/dev/dubhe/torchikoma/screen/TorchLauncherScreen.java
+++ src/main/java/dev/dubhe/torchikoma/screen/TorchLauncherScreen.java
@@ -4,29 +4,44 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import dev.dubhe.torchikoma.Torchikoma;
 import dev.dubhe.torchikoma.menu.TorchLauncherMenu;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+import java.util.List;
+import java.util.Optional;
+
 @OnlyIn(Dist.CLIENT)
 public class TorchLauncherScreen extends AbstractContainerScreen<TorchLauncherMenu> {
-    public static int temp = 0;
+    public static int temp1 = 0;
+    public static int temp2 = 0;
+    public static int temp3 = 0;
+    public static int temp4 = 0;
 
     private static final ResourceLocation GUN_BACKGROUND = Torchikoma.getId("textures/gui/gun.png");
     private static final ResourceLocation PLAYER_BACKGROUND = Torchikoma.getId("textures/gui/player.png");
     private static final int GUN_WIDTH = 178;
     private static final int GUN_HEIGHT = 58;
     private static final int PLAYER_WIDTH = 176;
-    private static final int PLAYER_HEIGHT = 90;
-    private static final ResourceLocation CONTAINER_BACKGROUND = new ResourceLocation("textures/gui/container/generic_54.png");
+    private static final int PLAYER_HEIGHT = 100;
+
+    private final ItemRenderer itemRenderer;
 
     public TorchLauncherScreen(TorchLauncherMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
+        itemRenderer = Minecraft.getInstance().getItemRenderer();
         this.titleLabelX = 50;
+        this.titleLabelY += 7;
     }
 
     @Override
@@ -37,17 +52,28 @@ public class TorchLauncherScreen extends AbstractContainerScreen<TorchLauncherMe
     }
 
     @Override
-    protected void renderLabels(PoseStack pPoseStack, int pMouseX, int pMouseY) {
-        this.font.draw(pPoseStack, this.title, (float)this.titleLabelX, (float)this.titleLabelY, 4210752);
+    protected void renderTooltip(PoseStack pPoseStack, int pX, int pY) {
+        super.renderTooltip(pPoseStack, pX, pY);
+        int x = (this.width - GUN_WIDTH) / 2 + 76;
+        int y = (this.height - GUN_HEIGHT - PLAYER_HEIGHT - 8) / 2 + 31;
+        if (pX >= x && pX <= x + 50 && pY >= y && pY <= y + 4) {
+            this.renderTooltip(pPoseStack, List.of(new TranslatableComponent("gui.torchikoma.gunpowder", this.menu.getGunpowder())), Optional.empty(), pX, pY);
+        }
     }
 
     @Override
     protected void renderBg(PoseStack pPoseStack, float pPartialTick, int pMouseX, int pMouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        int gunX = (this.width - GUN_WIDTH) / 2;
+        int gunY = (this.height - GUN_HEIGHT - PLAYER_HEIGHT - 8) / 2;
         RenderSystem.setShaderTexture(0, PLAYER_BACKGROUND);
-        this.blit(pPoseStack, (this.width - PLAYER_WIDTH) / 2, (this.height - PLAYER_HEIGHT) / 2 + 38, 0, 0, PLAYER_WIDTH, PLAYER_HEIGHT);
+        this.blit(pPoseStack, (this.width - PLAYER_WIDTH) / 2, gunY + GUN_HEIGHT + 8, 0, 0, PLAYER_WIDTH, PLAYER_HEIGHT);
         RenderSystem.setShaderTexture(0, GUN_BACKGROUND);
-        this.blit(pPoseStack, (this.width - GUN_WIDTH) / 2, (this.height - GUN_HEIGHT) / 2 - 61, 0, 0, GUN_WIDTH, GUN_HEIGHT);
+        this.blit(pPoseStack, gunX, gunY, 0, 0, GUN_WIDTH, GUN_HEIGHT);
+        this.blit(pPoseStack, gunX + 76, gunY + 31, GUN_WIDTH, 0, (int)(0.5D * this.menu.getGunpowder()), 4);
+        this.itemRenderer.renderAndDecorateItem(new ItemStack(Items.GUNPOWDER), gunX + 76, gunY + 38);
+        this.itemRenderer.renderAndDecorateItem(new ItemStack(Items.TORCH), gunX + 100, gunY + 36);
+        this.itemRenderer.renderAndDecorateItem(new ItemStack(Items.SOUL_TORCH), gunX + 104, gunY + 36);
     }
 }
