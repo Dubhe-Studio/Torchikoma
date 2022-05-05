313f0f926bb3f4f2d04275923bab8ce370fd1fe9
Gu_ZT <administration@ztxy666.cn>
2022-05-05 22:30:31 +0800

    Update BlocklightDetectorBlockEntity.java

--- src/main/java/dev/dubhe/torchikoma/block/entity/BlocklightDetectorBlockEntity.java
+++ src/main/java/dev/dubhe/torchikoma/block/entity/BlocklightDetectorBlockEntity.java
@@ -1,5 +1,7 @@
 package dev.dubhe.torchikoma.block.entity;
 
+import dev.dubhe.torchikoma.registry.MyBlockEntities;
+import dev.dubhe.torchikoma.registry.MyBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -7,7 +9,7 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public class BlocklightDetectorBlockEntity extends BlockEntity {
 
-    public BlocklightDetectorBlockEntity(BlockEntityType<?> pType, BlockPos pWorldPosition, BlockState pBlockState) {
-        super(pType, pWorldPosition, pBlockState);
+    public BlocklightDetectorBlockEntity(BlockPos pWorldPosition, BlockState pBlockState) {
+        super(MyBlockEntities.BLOCKLIGHT_DETECTOR, pWorldPosition, pBlockState);
     }
 }
