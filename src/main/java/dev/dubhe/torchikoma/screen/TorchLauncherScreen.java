80757c8ffa97bd34983bffc0774314739e4ee40a
Cjsah <2684117397@qq.com>
2022-05-06 11:48:37 +0800

    物品渲染

--- src/main/java/dev/dubhe/torchikoma/screen/TorchLauncherScreen.java
+++ src/main/java/dev/dubhe/torchikoma/screen/TorchLauncherScreen.java
@@ -1,13 +1,23 @@
 package dev.dubhe.torchikoma.screen;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
 import dev.dubhe.torchikoma.Torchikoma;
 import dev.dubhe.torchikoma.menu.TorchLauncherMenu;
+import dev.dubhe.torchikoma.registry.MyItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
@@ -35,12 +45,18 @@ public class TorchLauncherScreen extends AbstractContainerScreen<TorchLauncherMe
     private static final int PLAYER_HEIGHT = 100;
 
     private final ItemRenderer itemRenderer;
+    private int rotateAngle = 0;
 
     public TorchLauncherScreen(TorchLauncherMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
         itemRenderer = Minecraft.getInstance().getItemRenderer();
         this.titleLabelX = 50;
         this.titleLabelY += 7;
+    }
+
+    @Override
+    protected void containerTick() {
+        rotateAngle = (rotateAngle + temp1) % 360;
     }
 
     @Override
@@ -77,14 +93,50 @@ public class TorchLauncherScreen extends AbstractContainerScreen<TorchLauncherMe
             }
         }
         this.renderItemBg(pPoseStack, gunX + 52, gunY + 25, GUN_WIDTH, 4, 4);
+        this.renderBigItem(new ItemStack(MyItems.TORCH_LAUNCHER), gunX + 26, gunY + 33, 32.0F);
         this.itemRenderer.renderAndDecorateItem(new ItemStack(Items.GUNPOWDER), gunX + 76, gunY + 38);
         this.itemRenderer.renderAndDecorateItem(new ItemStack(Items.TORCH), gunX + 100, gunY + 36);
         this.itemRenderer.renderAndDecorateItem(new ItemStack(Items.SOUL_TORCH), gunX + 104, gunY + 36);
     }
 
+    @SuppressWarnings("SameParameterValue")
     private void renderItemBg(PoseStack pPoseStack, int x, int y, int u, int v, int index) {
         if (this.menu.isEmpty(index)) {
             this.blit(pPoseStack, x, y, u, v, 16, 16);
+        }
+    }
+
+    @SuppressWarnings({"deprecation", "SameParameterValue"})
+    private void renderBigItem(ItemStack pStack, int x, int y, float size) {
+        if (!pStack.isEmpty()) {
+            BakedModel bakedmodel = this.itemRenderer.getModel(pStack, null, Minecraft.getInstance().player, 0);
+            this.itemRenderer.textureManager.getTexture(TextureAtlas.LOCATION_BLOCKS).setFilter(false, false);
+            RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_BLOCKS);
+            RenderSystem.enableBlend();
+            RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+            PoseStack posestack = RenderSystem.getModelViewStack();
+            Quaternion quaternion = Vector3f.YP.rotationDegrees(this.rotateAngle);
+            posestack.pushPose();
+            posestack.translate(x, y, 100.0F);
+            posestack.scale(1.0F, -1.0F, 1.0F);
+            posestack.scale(size, size, size);
+            posestack.mulPose(quaternion);
+            RenderSystem.applyModelViewMatrix();
+            PoseStack poseStack = new PoseStack();
+            MultiBufferSource.BufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
+            boolean flag = !bakedmodel.usesBlockLight();
+            if (flag) {
+                Lighting.setupForFlatItems();
+            }
+            this.itemRenderer.render(pStack, ItemTransforms.TransformType.GUI, false, poseStack, bufferSource, 15728880, OverlayTexture.NO_OVERLAY, bakedmodel);
+            bufferSource.endBatch();
+            RenderSystem.enableDepthTest();
+            if (flag) {
+                Lighting.setupFor3DItems();
+            }
+            posestack.popPose();
+            RenderSystem.applyModelViewMatrix();
         }
     }
 }
