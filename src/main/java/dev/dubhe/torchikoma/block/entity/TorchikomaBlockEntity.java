9fee153240907ba104c3b2434ab7250636171052
Gu_ZT <administration@ztxy666.cn>
2022-05-09 19:06:59 +0800

    Update TorchikomaBlockEntity.java

--- src/main/java/dev/dubhe/torchikoma/block/entity/TorchikomaBlockEntity.java
+++ src/main/java/dev/dubhe/torchikoma/block/entity/TorchikomaBlockEntity.java
@@ -3,12 +3,19 @@ package dev.dubhe.torchikoma.block.entity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 
 public class TorchikomaBlockEntity extends RandomizableContainerBlockEntity{
@@ -18,24 +25,27 @@ public class TorchikomaBlockEntity extends RandomizableContainerBlockEntity{
         super(BlockEntityType.BARREL, pWorldPosition, pBlockState);
     }
 
+    @Nonnull
     @Override
     protected NonNullList<ItemStack> getItems() {
-        return null;
+        return this.items;
     }
 
     @Override
-    protected void setItems(NonNullList<ItemStack> pItemStacks) {
-
+    protected void setItems(@Nonnull NonNullList<ItemStack> pItemStacks) {
+        this.items = pItemStacks;
     }
 
+    @Nonnull
     @Override
     protected Component getDefaultName() {
-        return null;
+        return new TextComponent("Torchikoma");
     }
 
+    @NotNull
     @Override
-    protected AbstractContainerMenu createMenu(int pContainerId, Inventory pInventory) {
-        return null;
+    protected AbstractContainerMenu createMenu(int pContainerId, @Nonnull Inventory pInventory) {
+        return ChestMenu.threeRows(pContainerId, pInventory, this);
     }
 
     @Override
